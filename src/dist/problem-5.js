8a9113cde65622cd0e2a4ec4322ac1f975e3a1f2
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-11-08 15:18:34 +0600

    solved problem 5

---
+++ src/dist/problem-5.js
@@ -0,0 +1,6 @@
+"use strict";
+const getProperty = (object, propertyName) => {
+    return object[propertyName];
+};
+const person = { name: "Alice", age: 30 };
+// console.log(getProperty(person, "name"));
---
+++ src/problems/problem-5.ts
@@ -0,0 +1,9 @@
+const getProperty = <T, K extends keyof T>(
+  object: T,
+  propertyName: K
+): T[K] => {
+  return object[propertyName];
+};
+
+const person = { name: "Alice", age: 30 };
+// console.log(getProperty(person, "name"));
