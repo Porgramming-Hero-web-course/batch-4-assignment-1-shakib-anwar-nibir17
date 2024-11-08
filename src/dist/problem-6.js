a1b1e88a7a19f1cd0046e95b65c9d6f13c3057ae
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-11-08 16:21:14 +0600

    problem-6 solved

---
+++ src/dist/problem-6.js
@@ -0,0 +1,7 @@
+"use strict";
+const updateProfile = (profileObj, updateObj) => {
+    const updatedProfile = Object.assign(Object.assign({}, profileObj), updateObj);
+    return updatedProfile;
+};
+const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
+console.log(updateProfile(myProfile, { email: "newalice@example.com" }));
---
+++ src/problems/problem-6.ts
@@ -0,0 +1,16 @@
+interface Profile {
+  name: string;
+  age: number;
+  email: string;
+}
+
+const updateProfile = (
+  profileObj: Profile,
+  updateObj: Partial<Profile>
+): Profile => {
+  const updatedProfile: Profile = { ...profileObj, ...updateObj };
+  return updatedProfile;
+};
+
+const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
+// console.log(updateProfile(myProfile, { email: "newalice@example.com" }));
